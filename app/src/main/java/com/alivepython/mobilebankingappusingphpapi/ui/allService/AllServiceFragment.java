c7cf079bb355387f7eefdc44865eb17014c5baa3
shakawat <shakawat_hossain@outlook.com>
2022-03-08 00:19:51 +0600

    just testing code

--- app/src/main/java/com/alivepython/mobilebankingappusingphpapi/ui/allService/AllServiceFragment.java
+++ app/src/main/java/com/alivepython/mobilebankingappusingphpapi/ui/allService/AllServiceFragment.java
@@ -2,11 +2,15 @@ package com.alivepython.mobilebankingappusingphpapi.ui.allService;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.alivepython.mobilebankingappusingphpapi.R;
 
@@ -14,10 +18,13 @@ import com.alivepython.mobilebankingappusingphpapi.R;
 public class AllServiceFragment extends Fragment {
     View rootView;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView=inflater.inflate(R.layout.fragment_all_service, container, false);
+
+
 
 
 
