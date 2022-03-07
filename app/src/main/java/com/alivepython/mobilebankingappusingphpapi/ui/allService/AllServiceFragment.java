ec6b8789788f761331f62aec3939e6bdeb04940f
shakawat <shakawat_hossain@outlook.com>
2022-03-07 22:05:59 +0600

    working with navigation bar AllServiceFragment.java

---
+++ app/src/main/java/com/alivepython/mobilebankingappusingphpapi/ui/allService/AllServiceFragment.java
@@ -0,0 +1,26 @@
+package com.alivepython.mobilebankingappusingphpapi.ui.allService;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alivepython.mobilebankingappusingphpapi.R;
+
+
+public class AllServiceFragment extends Fragment {
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView=inflater.inflate(R.layout.fragment_all_service, container, false);
+
+
+
+        return rootView;
+    }
+}
